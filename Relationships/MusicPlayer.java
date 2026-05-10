c79430895444aa680b58dc086a2efd533ec51f30
Tanishq dhingra <dhingratanishq58@gmail.com>
2026-05-11 00:03:56 +0530

    Relationship added

---
+++ Relationships/MusicPlayer.java
@@ -0,0 +1,20 @@
+package Relationships;
+
+class Truck {
+    MusicPlayer player; //! aggregation
+
+    Truck(MusicPlayer player) {
+        this.player = player;
+    }
+
+    void start() {
+        System.out.println("Truck started");
+        player.play();
+    }
+}
+
+class MusicPlayer {
+    void play() {
+        System.out.println("Playing music...");
+    }
+}
